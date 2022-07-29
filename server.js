db5898015cd2530f38db56df93057438f53a29df
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-07-29 09:42:13 +0530

    basic req,res

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const http = require('http');
+const { getProducts } = require('./controllers/productControllers');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/api/products' && req.method === 'GET') {
+    getProducts(req, res);
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route Not Found' }));
+  }
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
