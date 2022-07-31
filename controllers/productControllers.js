0cbbda833b9b65007ed9443ade60332f3bfe7506
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-07-31 12:33:33 +0530

    node

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,5 +1,8 @@
 const Product = require('../models/productModel');
 
+// @description Gets All Products
+// @route GET /api/products
+
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -10,6 +13,43 @@ async function getProducts(req, res) {
   }
 }
 
+// @description Gets Single Product
+// @route GET /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product Not Found' }));
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// @description Creates Product
+// @route POST /api/products
+
+async function createProduct(req, res) {
+  try {
+    const product = {
+      title: 'Test Product',
+      description: 'This is my product',
+      price: 100,
+    };
+    const newProduct = await Product.create(product);
+    res.writeHead(201, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify(newProduct));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
+  createProduct,
 };
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const http = require('http');
-const { getProducts } = require('./controllers/productControllers');
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+} = require('./controllers/productControllers');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') {
     getProducts(req, res);
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === 'GET'
+  ) {
+    const id = req.url.split('/')[3];
+    getProduct(req, res, id);
+  } else if (req.url === '/api/products' && req.method === 'POST') {
+    createProduct(req, res);
   } else {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Route Not Found' }));
   }
 });
